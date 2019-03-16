ed5372754e7219927c65892af6acffc876435f12
Anders Rillbert <anders.rillbert@kutso.se>
2019-03-16 23:17:35 +0100

    remove trailing _ in id

--- lib/giblish/utils.rb
+++ lib/giblish/utils.rb
@@ -211,7 +211,8 @@ module Giblish
   # transforms strings to valid asciidoctor id strings
   def to_valid_id(input_str)
     id_str = "_#{input_str.strip.downcase}"
-    id_str.gsub(%r{[^a-z0-9]+},"_")
+    id_str.gsub!(%r{[^a-z0-9]+},"_")
+    id_str.chomp('_')
   end
   module_function :to_valid_id
 
