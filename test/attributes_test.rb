472a5cc08930028272a098be64197a49ee3451bf
Anders Rillbert <anders.rillbert@kutso.se>
2019-05-25 15:14:44 +0200

    improve helper util for tmp doc creation/removal

--- test/attributes_test.rb
+++ test/attributes_test.rb
@@ -9,6 +9,23 @@ class AttributesTest < Minitest::Test
 
   include Giblish::TestUtils
 
+  @@xref_str = <<~EOF
+      = Test xref handling
+      :numbered:
+      
+      == My First Section
+      
+      This is a ref to <<section_id_2>> via explicit id (it is not possible to use automatic id 
+      when an explicit id is set).
+
+      [[section_id_2]]      
+      == My Second Section
+      
+      This is a reference to <<_my_first_section>> via automatic id.
+
+      NOTE: This is to test the note-caption attrib
+  EOF
+
   def setup
 
     # create dir for test docs
@@ -25,92 +42,84 @@ class AttributesTest < Minitest::Test
   end
 
   def test_xref_short_style_via_attrib_flag
-
-    xref_doc = <<~EOF
-      = Test xref handling
-      :numbered:
-      
-      == My First Section
-      
-      This is a ref to <<section_id_2>> via explicit id (it is not possible to use automatic id 
-      when an explicit id is set).
-
-      [[section_id_2]]      
-      == My Second Section
-      
-      This is a reference to <<_my_first_section>> via automatic id.
-    EOF
     expected_ref_text = ["Section 2",
                          "Section 1"]
 
-    # write doc to file
-    file = File.open(@src_root + "/tst_xref_styles.adoc", "w")
-    file.puts xref_doc
-    file.close
+    TmpDocDir.open do |tmp_docs|
+      # act on the input data
+      adoc_filename = tmp_docs.add_doc_from_str @@xref_str
+      args = ["--log-level", "info",
+              "-a", "xrefstyle=short",
+              tmp_docs.dir,
+              tmp_docs.dir]
+      Giblish.application.run_with_args args
 
-    # run giblish on the doc
-    args = ["--log-level", "info",
-            "-a", "xrefstyle=short",
-            @src_root,
-            @dst_root]
-    Giblish.application.run_with_args args
-
-    # parse the generated html
-    outfile = "#{@dst_root}/tst_xref_styles.html"
-    handle = File.open(outfile)
-    document = Oga.parse_html(handle)
-
-    # assert that the expected matches the actual
-    ref_nr = 0
-    document.css('a').each do |n|
-      assert_equal expected_ref_text[ref_nr], n.text
-      ref_nr+=1
+      # assert that the expected matches the actual
+      tmp_docs.check_result_html adoc_filename do |html_tree|
+        ref_nr = 0
+        html_tree.css('a').each do |n|
+          assert_equal expected_ref_text[ref_nr], n.text
+          ref_nr+=1
+        end
+      end
     end
   end
 
   def test_xref_full_style_via_attrib_flag
-
-    xref_doc = <<~EOF
-      = Test xref handling
-      :numbered:
-      
-      == My First Section
-      
-      This is a ref to <<section_id_2>> via explicit id (it is not possible to use automatic id 
-      when an explicit id is set).
-
-      [[section_id_2]]      
-      == My Second Section
-      
-      This is a reference to <<_my_first_section>> via automatic id.
-    EOF
-
     # note the 'smart quotes'...
     expected_ref_text = ['Section 2, “My Second Section”',
                          'Section 1, “My First Section”']
 
-    # write doc to file
-    file = File.open(@src_root + "/tst_xref_styles.adoc", "w")
-    file.puts xref_doc
-    file.close
+    TmpDocDir.open do |tmp_docs|
+      # act on the input data
+      adoc_filename = tmp_docs.add_doc_from_str @@xref_str
+      args = ["--log-level", "info",
+              "-a", "xrefstyle=full",
+              tmp_docs.dir,
+              tmp_docs.dir]
+      Giblish.application.run_with_args args
 
-    # run giblish on the doc
-    args = ["--log-level", "info",
-            "-a", "xrefstyle=full",
-            @src_root,
-            @dst_root]
-    Giblish.application.run_with_args args
+      # assert that the expected matches the actual
+      tmp_docs.check_result_html adoc_filename do |html_tree|
+        ref_nr = 0
+        html_tree.css('a').each do |n|
+          assert_equal expected_ref_text[ref_nr], n.text
+          ref_nr+=1
+        end
+      end
+    end
+  end
 
-    # parse the generated html
-    outfile = "#{@dst_root}/tst_xref_styles.html"
-    handle = File.open(outfile)
-    document = Oga.parse_html(handle)
 
-    # assert that the expected matches the actual
-    ref_nr = 0
-    document.css('a').each do |n|
-      assert_equal expected_ref_text[ref_nr], n.text
-      ref_nr+=1
+  def test_xref_and_note_attrib
+    # note the 'smart quotes'...
+    expected_ref_text = ['Section 2, “My Second Section”',
+                         'Section 1, “My First Section”']
+    expected_note_text = "TestNote"
+
+    TmpDocDir.open do |tmp_docs|
+      # act on the input data
+      adoc_filename = tmp_docs.add_doc_from_str @@xref_str
+      args = ["--log-level", "info",
+              "-a", "xrefstyle=full",
+              "-a", "note-caption=#{expected_note_text}",
+              tmp_docs.dir,
+              tmp_docs.dir]
+      Giblish.application.run_with_args args
+
+      # assert that the expected matches the actual
+      tmp_docs.check_result_html adoc_filename do |html_tree|
+        ref_nr = 0
+        # assert that the xref==full behaves as expected
+        html_tree.css('a').each do |n|
+          assert_equal expected_ref_text[ref_nr], n.text
+          ref_nr+=1
+        end
+
+        html_tree.css('.title').each do |n|
+          assert_equal expected_note_text, n.text
+        end
+      end
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -38,5 +38,58 @@ module Giblish
       FileUtils.rm_r @dst_root
 
     end
+
+    class TmpDocDir
+      attr_reader :adoc_filename
+      attr_reader :dir
+
+      # enable a user of this class to do things like:
+      # TmpDocDir.open do |doc_dir|
+      # ...
+      # end
+      # and be sure that the dir is deleted afterwards
+      def self.open(preserve = false)
+        instance = TmpDocDir.new
+        begin
+          yield instance
+        ensure
+          instance.close unless preserve
+        end
+      end
+
+      def close
+        FileUtils.remove_entry @dir
+      end
+
+      def initialize
+        @dir = Dir.mktmpdir
+        @src_files = []
+      end
+
+      # usage:
+      # tmp_docs.check_result_html adoc_filename do |html_tree|
+      #   <process the html tree>
+      # end
+      def check_result_html filename
+        html_file = Pathname.new(filename.gsub(/\.adoc$/,'.html'))
+
+        # parse the generated html and return the result to the user
+        handle = File.open(html_file)
+        document = Oga.parse_html(handle)
+        yield document
+      end
+
+      def add_doc_from_str(doc_str)
+        # create a temp file name
+        adoc_file = Tempfile.new(['gib_tst_','.adoc'],@dir)
+
+        # write doc to file and close
+        adoc_file.puts doc_str
+        adoc_file.close
+        @src_files << adoc_file
+        adoc_file.path
+      end
+    end
+
   end
 end
