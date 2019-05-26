0485c399366b4a759ba9442d2436ed7e40cf9936
Anders Rillbert <anders.rillbert@kutso.se>
2019-05-26 09:28:50 +0200

    use common TestUtils in more tests

--- test/depgraph_test.rb
+++ test/depgraph_test.rb
@@ -4,26 +4,15 @@ require_relative "../lib/giblish/utils.rb"
 require_relative "../lib/giblish/docid.rb"
 
 class DepGraphTests < Minitest::Test
+  include Giblish::TestUtils
+
   def setup
-    # setup logging
-    Giblog.setup
-
-    # find test directory path
-    @testdir_path = File.expand_path(File.dirname(__FILE__))
-
-    @src_root = "#{@testdir_path}/../data/testdocs"
-    @dst_root = "#{@testdir_path}/../testoutput"
-    FileUtils.mkdir_p @dst_root
-
-    # Instanciate a path manager with
-    # source root ==  .../giblish/data/testdocs and
-    # destination root == .../giblish/test/testoutput
-    @paths = Giblish::PathManager.new(@src_root,
-                                      @dst_root)
+    @src_root = "#{File.expand_path(File.dirname(__FILE__))}/../data/testdocs"
+    setup_log_and_paths
   end
 
   def teardown
-    FileUtils.rm_r @dst_root
+    teardown_log_and_paths dry_run: false
   end
 
   def test_graph_is_created_depending_on_graphviz
--- test/docid_test.rb
+++ test/docid_test.rb
@@ -3,39 +3,23 @@ require_relative "../lib/giblish/utils.rb"
 require_relative "../lib/giblish/docid.rb"
 
 class DocidCollectorTest < Minitest::Test
+  include Giblish::TestUtils
 
   def setup
-    # setup logging
-    Giblog.setup
-
-    # find test directory path
-    @testdir_path = File.expand_path(File.dirname(__FILE__))
-
-    @src_root = "#{@testdir_path}/../data/testdocs"
-    @dst_root = "#{@testdir_path}/../testoutput"
-    FileUtils.mkdir_p @dst_root
-
-    # Instanciate a path manager with
-    # source root ==  .../giblish/data/testdocs and
-    # destination root == .../giblish/test/testoutput
-    @paths = Giblish::PathManager.new(@src_root,
-                                      @dst_root)
+    @src_root = "#{File.expand_path(File.dirname(__FILE__))}/../data/testdocs"
+    setup_log_and_paths
   end
 
   def teardown
-    # FileUtils.rm_r @dst_root
+    teardown_log_and_paths dry_run: true
   end
 
   def test_collect_docids
-    args = ["--log-level",
-            "info",
-            "-d",
+    args = ["-d",
             @src_root,
             @dst_root]
     Giblish.application.run_with_args args
-
-#    src_root_path = @paths.src_root_abs + "wellformed/docidtest"
-    # src_root_path = @paths.src_root_abs
-
+    status = Giblish.application.run_with_args args
+    assert_equal 0, status
   end
 end
