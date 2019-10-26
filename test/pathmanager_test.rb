a4db7f3acfaf742d4f4f41f997d8bf29104cfb67
Anders Rillbert <anders.rillbert@kutso.se>
2019-10-26 20:24:57 +0200

    add more path manager tests

--- test/pathmanager_test.rb
+++ test/pathmanager_test.rb
@@ -11,10 +11,16 @@ class PathManagerTest < Minitest::Test
     # create a file area to hold src and dst trees
     @topdir = this_dir.join("_pathtesttopdir")
     FileUtils.mkdir_p(@topdir)
-    @src_root = @topdir.join("src_topdir")
+    @src_root_rel = Pathname.new("src_topdir")
+    @src_root = @topdir.join(@src_root_rel)
     FileUtils.mkdir_p(@src_root)
-    @dst_root= @topdir.join("dst_topdir")
+    @dst_root_rel = Pathname.new("dst_topdir")
+    @dst_root= @topdir.join(@dst_root_rel)
     FileUtils.mkdir_p(@dst_root)
+
+    @src_root_file_path = @src_root.join("file1.adoc")
+    f = File.new(@src_root_file_path, "w")
+    f.close
 
     @resource_rel = Pathname.new("resources")
     @resource_dir = @src_root.join(@resource_rel)
@@ -27,6 +33,9 @@ class PathManagerTest < Minitest::Test
     @src_subdir1_abs = @src_root.join(@src_subdir1).realpath
     @src_subsub1_abs = @src_root.join(@src_subsub1).realpath
 
+    @src_subsub_file_path = @src_subsub1_abs.join("file2.adoc")
+    f = File.new(@src_subsub_file_path, "w")
+    f.close
   end
 
   def teardown
@@ -54,6 +63,50 @@ class PathManagerTest < Minitest::Test
   def test_reldir_from_src_root
     p = Giblish::PathManager.new(@src_root, @dst_root)
     assert_equal @src_subsub1, p.reldir_from_src_root(@src_subsub1_abs)
+    assert_equal Pathname.new("."), p.reldir_from_src_root(@src_root)
+    assert_equal Pathname.new(".."), p.reldir_from_src_root(@src_root.join(".."))
 
+    assert_equal @src_subsub1, p.reldir_from_src_root(@src_subsub1_abs.join("fakefile.txt"))
   end
+
+  def test_reldir_to_src_root
+    p = Giblish::PathManager.new(@src_root, @dst_root)
+    assert_equal Pathname.new("../.."), p.reldir_to_src_root(@src_subsub1_abs)
+    assert_equal Pathname.new("."), p.reldir_to_src_root(@src_root)
+    assert_equal @src_root.relative_path_from(@topdir), p.reldir_to_src_root(@src_root.join(".."))
+
+    assert_equal Pathname.new("../.."), p.reldir_to_src_root(@src_subsub1_abs.join("fakefile.txt"))
+    assert_equal Pathname.new("../.."), p.reldir_to_src_root(@src_subsub1_abs.to_s)
+  end
+
+  def test_reldir_from_dst_root
+    p = Giblish::PathManager.new(@src_root, @dst_root)
+    assert_equal Pathname.new("..").join(@src_root_rel).join(@src_subsub1), p.reldir_from_dst_root(@src_subsub1_abs)
+    assert_equal Pathname.new("..").join(@src_root_rel), p.reldir_from_dst_root(@src_root)
+    assert_equal Pathname.new(".."), p.reldir_from_dst_root(@src_root.join(".."))
+
+    assert_equal Pathname.new("..").join(@src_root_rel).join(@src_subsub1), p.reldir_from_dst_root(@src_subsub1_abs.join("fakefile.txt"))
+  end
+
+  def test_reldir_to_dst_root
+    p = Giblish::PathManager.new(@src_root, @dst_root)
+    assert_equal Pathname.new("../../..").join(@dst_root_rel), p.reldir_to_dst_root(@src_subsub1_abs)
+    assert_equal Pathname.new("..").join(@dst_root_rel), p.reldir_to_dst_root(@src_root)
+    assert_equal @dst_root_rel, p.reldir_to_dst_root(@src_root.join(".."))
+
+    assert_equal Pathname.new("../../..").join(@dst_root_rel), p.reldir_to_dst_root(@src_subsub1_abs.join("fakefile.txt"))
+  end
+
+  def test_dst_abs_from_src_abs
+    p = Giblish::PathManager.new(@src_root, @dst_root)
+    assert_equal Pathname.new(@dst_root).join(@src_subsub1), p.dst_abs_from_src_abs(@src_subsub1_abs)
+    assert_equal Pathname.new(@dst_root).join(@src_subsub1), p.dst_abs_from_src_abs(@src_subsub_file_path)
+  end
+
+  def test_relpath_to_dir_after_generate
+    p = Giblish::PathManager.new(@src_root, @dst_root)
+    assert_equal Pathname.new("../.."), p.relpath_to_dir_after_generate(@src_subsub_file_path,@dst_root)
+    assert_equal Pathname.new("."), p.relpath_to_dir_after_generate(@src_root_file_path,@dst_root)
+  end
+
 end
