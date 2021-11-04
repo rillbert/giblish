bcc68b29b17b02452e2bc10aad29b122aebafa4e
Anders Rillbert <anders.rillbert@kutso.se>
2021-11-04 15:59:15 +0100

    draft of webhook trigger

--- apps/gh_webhook_trigger/gh_webhook_trigger.rb
+++ apps/gh_webhook_trigger/gh_webhook_trigger.rb
@@ -1,7 +1,9 @@
-require 'sinatra'
-require 'json'
+require "sinatra"
+require "json"
 
-post '/docgen' do
+# giblish -a xrefstyle=basic --copy-asset-folders "_assets$" -g "${TRIGGERING_REFS_REGEX}" -r scripts/resources -s giblish docs "${DST_DIR}"
+
+post "/payload" do
   push = JSON.parse(request.body.read, symbolized_names: true)
   puts "JSON from github: #{push.inspect}"
 end
